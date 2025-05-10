86189cd9420e97e6aa0f3688bbac980c6470b700
Twisuki <Twisuki@outlook.com>
2025-05-10 13:12:32 +0800

    周 / date

--- src/script/crawler.js
+++ src/script/crawler.js
@@ -5,6 +5,7 @@ import {dataLoader} from "./data.js";
 const config = await dataLoader();
 
 const JWURL = "http://hdjw.hnu.edu.cn/Njw2017/index.html#/student";
+const myDate = new Date();
 
 async function getOriginTimeTable () {
 	const originTimeTable = [
@@ -60,5 +61,52 @@ async function getOriginTimeTable () {
 export async function getTimeTable () {
 	const originTimeTable = await getOriginTimeTable();
 
+	const semesterStart = new Date(config.semesterStart);
+	const weekNum = (function () {
+		const daysDiff = (myDate.getDate() - semesterStart) / (1000 * 60 * 60 * 24);
+		return Math.abs(Math.floor(daysDiff / 7));
+	})();
 
+	console.log(weekNum);
+
+	const timeTable = [
+		[{}, {}, {}, {}, {}],
+		[
+			{"name": "普通物理AⅠ", "class": "综417", "week": "1-16"},
+			{"name": "高等数学AⅡ", "class": "综504", "week": "1-16"},
+			{"name": "体育Ⅱ", "class": "", "week": "1-16"},
+			{},
+			{}
+		],
+		[{"name": "计算与人工智能概论A", "class": "综B102", "week": "2-16"}, {}, {}, {}, {}],
+		[
+			{},
+			{"name": "高等数学AⅡ", "class": "综504", "week": "1-16"},
+			{"name": "计算与人工智能概论A", "class": "综215", "week": "10-16"},
+			{},
+			{"name": "普通物理实验AⅠ", "class": "", "week": "2,4,6,8,10,12,14,16"}
+		],
+		[
+			{},
+			{"name": "普通物理AⅠ", "class": "综417", "week": "1-16"},
+			{"name": "大学英语Ⅱ", "class": "研A209", "week": "1-16"},
+			{"name": "中国近现代史纲要", "class": "研A403", "week": "1-16"},
+			{}
+		],
+		[
+			{"name": "高等数学AⅡ", "class": "综504", "week": "1-16"},
+			{"name": "人工智能导论", "class": "综115", "week": "1-7,9-16"},
+			{"name": "计算与人工智能概论A", "class": "综515", "week": "1-16"},
+			{},
+			{}
+		],
+		[
+			{},
+			{},
+			{},
+			{},
+			{}
+		]
+	];
+	return timeTable;
 }
