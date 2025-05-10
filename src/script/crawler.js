2ef4cadb4e7c8ac75b51f0cd81ed71637fb5612e
Twisuki <Twisuki@outlook.com>
2025-05-10 13:57:47 +0800

    课程表分析

--- src/script/crawler.js
+++ src/script/crawler.js
@@ -59,8 +59,6 @@ async function getOriginTimeTable () {
 }
 
 export async function getTimeTable () {
-	const originTimeTable = await getOriginTimeTable();
-
 	// 计算周数
 	const semesterStart = new Date(config.semesterStart);
 	const weekNum = (function () {
@@ -69,47 +67,86 @@ export async function getTimeTable () {
 		return Math.abs(Math.floor(daysDiff / 7)) + 1;
 	})();
 
+	// 处理课表
+	const originTimeTable = await getOriginTimeTable();
+	const timeTable = [];
+
+	for (const originDayList of originTimeTable) {
+		const dayList = [];
+		for (const originCell of originDayList) {
+			if (! Array.isArray(originCell)) {
+				// 课程对象, 分析周数
+				if (isThisWeek(originCell.week, weekNum)) {
+					const cell = {"name": originCell.name, "class": originCell.class};
+					dayList.push(cell);
+				} else {
+					dayList.push({});
+				}
+			} else {
+				// 数组, 分析周数
+				let flag = false;
+				for (const obj of originCell) {
+					if (isThisWeek(obj.week, weekNum)) {
+						const cell = {"name": originCell.name, "class": originCell.class};
+						dayList.push(cell);
+						flag = true;
+						break;
+					}
+				}
+				// 空对象占位
+				if (! flag) {
+					dayList.push({});
+				}
+			}
+		}
+
+		if (dayList.length === 7) {
+			timeTable.push(dayList);
+		} else {
+			console.error("课程表解析错误! 课程大小: ", dayList.length);
+		}
+	}
 
 
-	const timeTable = [
-		[{}, {}, {}, {}, {}],
-		[
-			{"name": "普通物理AⅠ", "class": "综417"},
-			{"name": "高等数学AⅡ", "class": "综504"},
-			{"name": "体育Ⅱ", "class": ""},
-			{},
-			{}
-		],
-		[{"name": "计算与人工智能概论A", "class": "综B102"}, {}, {}, {}, {}],
-		[
-			{},
-			{"name": "高等数学AⅡ", "class": "综504"},
-			{"name": "计算与人工智能概论A", "class": "综215"},
-			{},
-			{"name": "普通物理实验AⅠ", "class": ""}
-		],
-		[
-			{},
-			{"name": "普通物理AⅠ", "class": "综417"},
-			{"name": "大学英语Ⅱ", "class": "研A209"},
-			{"name": "中国近现代史纲要", "class": "研A403"},
-			{}
-		],
-		[
-			{"name": "高等数学AⅡ", "class": "综504"},
-			{"name": "人工智能导论", "class": "综115"},
-			{"name": "计算与人工智能概论A", "class": "综515"},
-			{},
-			{}
-		],
-		[
-			{},
-			{},
-			{},
-			{},
-			{}
-		]
-	];
+	// const timeTable = [
+	// 	[{}, {}, {}, {}, {}],
+	// 	[
+	// 		{"name": "普通物理AⅠ", "class": "综417"},
+	// 		{"name": "高等数学AⅡ", "class": "综504"},
+	// 		{"name": "体育Ⅱ", "class": ""},
+	// 		{},
+	// 		{}
+	// 	],
+	// 	[{"name": "计算与人工智能概论A", "class": "综B102"}, {}, {}, {}, {}],
+	// 	[
+	// 		{},
+	// 		{"name": "高等数学AⅡ", "class": "综504"},
+	// 		{"name": "计算与人工智能概论A", "class": "综215"},
+	// 		{},
+	// 		{"name": "普通物理实验AⅠ", "class": ""}
+	// 	],
+	// 	[
+	// 		{},
+	// 		{"name": "普通物理AⅠ", "class": "综417"},
+	// 		{"name": "大学英语Ⅱ", "class": "研A209"},
+	// 		{"name": "中国近现代史纲要", "class": "研A403"},
+	// 		{}
+	// 	],
+	// 	[
+	// 		{"name": "高等数学AⅡ", "class": "综504"},
+	// 		{"name": "人工智能导论", "class": "综115"},
+	// 		{"name": "计算与人工智能概论A", "class": "综515"},
+	// 		{},
+	// 		{}
+	// 	],
+	// 	[
+	// 		{},
+	// 		{},
+	// 		{},
+	// 		{},
+	// 		{}
+	// 	]
+	// ];
 	return timeTable;
 }
 
