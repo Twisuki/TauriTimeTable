7bd57401c98117c9cf3f6e3f2588a5154256ed08
Twisuki <Twisuki@outlook.com>
2025-05-10 14:05:59 +0800

    bug fixed

--- src/script/crawler.js
+++ src/script/crawler.js
@@ -74,20 +74,13 @@ export async function getTimeTable () {
 	for (const originDayList of originTimeTable) {
 		const dayList = [];
 		for (const originCell of originDayList) {
-			if (! Array.isArray(originCell)) {
-				// 课程对象, 分析周数
-				if (isThisWeek(originCell.week, weekNum)) {
-					const cell = {"name": originCell.name, "class": originCell.class};
-					dayList.push(cell);
-				} else {
-					dayList.push({});
-				}
-			} else {
+			// 按类别分析
+			if (Array.isArray(originCell)) {
 				// 数组, 分析周数
 				let flag = false;
 				for (const obj of originCell) {
 					if (isThisWeek(obj.week, weekNum)) {
-						const cell = {"name": originCell.name, "class": originCell.class};
+						const cell = {"name": obj.name, "class": obj.class};
 						dayList.push(cell);
 						flag = true;
 						break;
@@ -97,72 +90,43 @@ export async function getTimeTable () {
 				if (! flag) {
 					dayList.push({});
 				}
+			} else if (JSON.stringify(originCell) !== "{}") {
+				// 课程对象且非空, 分析周数
+				if (isThisWeek(originCell.week, weekNum)) {
+					const cell = {"name": originCell.name, "class": originCell.class};
+					dayList.push(cell);
+				} else {
+					dayList.push({});
+				}
+			} else {
+				// 空对象
+				dayList.push({});
 			}
 		}
 
-		if (dayList.length === 7) {
+		if (dayList.length === 5) {
 			timeTable.push(dayList);
 		} else {
 			console.error("课程表解析错误! 课程大小: ", dayList.length);
 		}
 	}
 
-
-	// const timeTable = [
-	// 	[{}, {}, {}, {}, {}],
-	// 	[
-	// 		{"name": "普通物理AⅠ", "class": "综417"},
-	// 		{"name": "高等数学AⅡ", "class": "综504"},
-	// 		{"name": "体育Ⅱ", "class": ""},
-	// 		{},
-	// 		{}
-	// 	],
-	// 	[{"name": "计算与人工智能概论A", "class": "综B102"}, {}, {}, {}, {}],
-	// 	[
-	// 		{},
-	// 		{"name": "高等数学AⅡ", "class": "综504"},
-	// 		{"name": "计算与人工智能概论A", "class": "综215"},
-	// 		{},
-	// 		{"name": "普通物理实验AⅠ", "class": ""}
-	// 	],
-	// 	[
-	// 		{},
-	// 		{"name": "普通物理AⅠ", "class": "综417"},
-	// 		{"name": "大学英语Ⅱ", "class": "研A209"},
-	// 		{"name": "中国近现代史纲要", "class": "研A403"},
-	// 		{}
-	// 	],
-	// 	[
-	// 		{"name": "高等数学AⅡ", "class": "综504"},
-	// 		{"name": "人工智能导论", "class": "综115"},
-	// 		{"name": "计算与人工智能概论A", "class": "综515"},
-	// 		{},
-	// 		{}
-	// 	],
-	// 	[
-	// 		{},
-	// 		{},
-	// 		{},
-	// 		{},
-	// 		{}
-	// 	]
-	// ];
 	return timeTable;
 }
 
 // 处理周
 function isThisWeek (week, weekNum) {
 	// 去除空格
-	const normalizedWeek = week.replace(/\s/g, '');
+	const normalizedWeek = week.replace(/\s/g, "");
 
 	// 逗号分割
-	const conditions = normalizedWeek.split(',');
+	const conditions = normalizedWeek.split(",");
 
 	// 逐个检查
 	for (const condition of conditions) {
-		if (condition.includes('-')) {
+		if (condition.includes("-")) {
 			// 范围处理
-			const [start, end] = condition.split('-').map(Number);
+			const [start, end] = condition.split("-").map(Number);
 			if (weekNum >= start && weekNum <= end) {
 				return true;
 			}
