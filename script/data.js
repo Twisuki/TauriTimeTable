190928013a960dd1fd5afdb06235c5680e6c4690
Twisuki <Twisuki@outlook.com>
2025-05-12 23:27:11 +0800

    localStorage读取写入

--- script/data.js
+++ script/data.js
@@ -1,10 +1,31 @@
-export async function dataLoader () {
-	try {
-		const response = await fetch("../config/config.json");
-		return response.json();
-	} catch (error) {
-		console.error("配置加载失败: ", error);
-		return null;
-	}
+// 使用LocalStorage保存数据
+var data = {};
+if (localStorage.getItem("LocalData") !== null) {
+	data = JSON.parse(localStorage.getItem("LocalData"));
+} else {
+	const defaultData = {
+		"user": "Default0",
+		"semesterStart": "2025-2-16",
+		"timeTable": [
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}],
+			[{}, {}, {}, {}, {}]
+		]
+	};
+	console.log("LocalData不存在, 初始化");
+	localStorage.setItem("LocalData", JSON.stringify(defaultData));
 }
 
+export function dataLoader () {
+	return data;
+}
+
+export function dataEdit (newData) {
+	localStorage.setItem("LocalData", JSON.stringify(newData));
+	console.log(`LocalData以设为{newData}`);
+	data = JSON.parse(localStorage.getItem("LocalData"));
+}
